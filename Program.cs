53660740547fdb909b0996652e357a8ac8f78a30
pritpalkaur05@gmail.com <pritpalkaur05@gmail.com>
2020-05-16 23:26:01 +0800

    code check in

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {   // Open the text file using a stream reader.
+
+                List<string> words = new List<string>();
+
+                using (StreamReader sr = new System.IO.StreamReader("TestFile.txt"))
+                {
+
+                    while(!sr.EndOfStream)
+                    { 
+                      words.Add(sr.ReadLine());
+                    }
+                    
+                }
+
+                words.Sort();
+                foreach (var line in words)
+                {
+                    Console.WriteLine(line);
+                }
+             
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
